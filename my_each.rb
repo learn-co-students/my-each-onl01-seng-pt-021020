f5bc7edec8c6309b982f27da7f1a99382e37d6b3
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-02-19 19:42:45 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,9 @@
 def my_each(collection)
-  new_collection = []
     i = 0 
-    while i <= collection.length 
-     yield(collection[i])
+    while i < collection.length 
+    yield(collection[i])
     i += 1 
+    
 end 
-
-
+collection
 end
