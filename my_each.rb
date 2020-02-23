8571afa7b2acb1f67a79a9657ffd8b0f447e9941
Steven Prindle <flankbusiness@gmail.com>
2020-02-23 03:59:31 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+#def my_each # put argument(s) here
   # code here
+#end
+
+def my_each(arg)
+  i = 0
+  while i < arg.length
+    yield(arg[i])
+    i += 1
+  end
+  arg
 end
